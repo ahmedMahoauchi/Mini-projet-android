7c648d5c174c079ad0514c46097c3079459d9050
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2022-11-09 19:16:52 +0100

    Login ui/ux

--- app/src/main/java/com/example/miniprojetandroid/SignInActivity.kt
+++ app/src/main/java/com/example/miniprojetandroid/SignInActivity.kt
@@ -11,12 +11,7 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        val signpBtn = findViewById<LinearLayout>(R.id.linearLayout)
 
-        signpBtn.setOnClickListener{
-            intent = Intent(this,SignUpActivity::class.java)
-            startActivity(intent)
-        }
     }
 
 
